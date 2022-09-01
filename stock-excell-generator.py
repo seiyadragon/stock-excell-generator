4d5e284dacb2e87cf840bebd1b6629ba49d3f91f
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-09-01 17:37:39 -0500

    Update stock-excell-generator.py

--- stock-excell-generator.py
+++ stock-excell-generator.py
@@ -3,33 +3,31 @@ from concurrent.futures import thread
 from pickle import TRUE
 import sys
 import yfinance as yf
-from threading import Thread
 
 stock_list = []
 
 class Stock:
     name = None
     data = None
-    price = None
-    actions = None
-    dividends = None
-    earnings = None
 
     def __init__(self, data, name) -> None:
         self.data = data
         self.name = name
 
-        for (index, text) in enumerate(data):
-            print()
+        print("$" + str(self.data["regularMarketPrice"]))
 
 def main():
     file = open("./stocklist.txt", "r")
     stock_name_list = file.read().split("\n")
 
     for (index, value) in enumerate(stock_name_list):
-        tmp = yf.Ticker(value)
         print("Downloading data for: " + value)
-        stock_list.append(Stock(tmp.info, value))
+
+        tmp = yf.Ticker(value)
+        info = tmp.info
+
+        if info["regularMarketPrice"] != None:
+            stock_list.append(Stock(info, value))
         
 
 if __name__ == "__main__":
