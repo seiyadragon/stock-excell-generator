df759e839c59ed034dc5097d605628d66abdd9db
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-09-01 19:06:46 -0700

    Update stock-excell-generator.py

--- stock-excell-generator.py
+++ stock-excell-generator.py
@@ -2,32 +2,31 @@ from cmath import log
 from concurrent.futures import thread
 from pickle import TRUE
 import sys
+from tkinter.tix import MAX
 import yfinance as yf
 
 stock_list = []
 
 class Stock:
+    ticker = None
     name = None
     data = None
 
-    def __init__(self, data, name) -> None:
-        self.data = data
+    def __init__(self, name) -> None:
         self.name = name
 
-        print("$" + str(self.data["regularMarketPrice"]))
+        ticker = yf.Ticker(self.name)
+        self.data = ticker.history(period=MAX)
+
+        print(self.data)
 
 def main():
     file = open("./stocklist.txt", "r")
     stock_name_list = file.read().split("\n")
 
     for (index, value) in enumerate(stock_name_list):
-        print("Downloading data for: " + value)
-
-        tmp = yf.Ticker(value)
-        info = tmp.info
-
-        if info["regularMarketPrice"] != None:
-            stock_list.append(Stock(info, value))
+        print("\nFetching data for: " + value)
+        stock_list.append(Stock(value))
         
 
 if __name__ == "__main__":
