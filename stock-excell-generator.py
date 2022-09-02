a7f5a8d04f229fdf0d6014fd2ca03c3d07a7c05b
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-09-02 13:44:46 -0700

    Update stock-excell-generator.py

--- stock-excell-generator.py
+++ stock-excell-generator.py
@@ -1,63 +1,98 @@
-from cmath import log
+from cmath import isnan, log
 from concurrent.futures import thread
 from inspect import getargvalues
 from pickle import FALSE, TRUE
 import sys
 from tkinter.tix import MAX
 import yfinance as yf
+import math
 
 stock_list = []
 
 class Stock:
     ticker = None
     name = None
+    years = None
     data = None
-    percent_up = None
 
-    def __init__(self, name) -> None:
+    current_price = None
+    average_growth = None
+    average_dividend = None
+    price_earning_ratio = None
+
+    total_yield = None
+    potential_loss = None
+    risk = None
+
+    def __init__(self, name, years) -> None:
         self.name = name
+        self.years = years
 
-        ticker = yf.Ticker(self.name)
-        self.data = ticker.history(period=MAX, interval="1mo")
+        print("\nFetching data for: " + self.name)
+
+        self.ticker = yf.Ticker(self.name)
+        self.data = self.ticker.history(period=MAX, interval="1mo")
         self.data = self.data[::-1]
 
         if self.data.empty:
             stock_list.pop(len(stock_list) - 1)
             return
+        
+        self.current_price = float(self.data.iloc[0].iat[3])
+        print(self.name + ": $" + str(self.current_price))
 
-        self.percent_up = self.getAverageTrend(5)
+        self.average_growth = self.getAverageGrowth()
+        self.average_dividend = self.getAverageDividend()
+        print(self.ticker.actions)
 
         print(self.name + ": Was retrieved correctly!")
-        print(self.name + ": " + str(self.percent_up) + "%")
 
-    def getAverageTrend(self, years):
+    def getAverageGrowth(self):
+        years = self.years
         prices = []
-        has_enough_data = False
         prices_calculated = []
         result = 0
-
-        for i in range(years * 12):
+        range_offset = 0
+        
+        while_index = 0
+        while len(prices) < years * 12:
             try:
-                prices.append(self.data.iloc[i].iat[3])
-                has_enough_data = True
-            except:
-                print(self.name + ": Does not have sufficient data!")
-                has_enough_data = False
+                tmp = float(self.data.iloc[while_index].iat[3])
+            except: break
+            
+            while_index += 1
+
+            if not math.isnan(tmp):
+                prices.append(tmp)
 
         print(self.name + ": Months found --" + str(len(prices)))
 
-        if has_enough_data:
+        if len(prices) >= years * 12:
             for i in range(years):
-                if prices[i] - prices[i + 12 * (years - 1)] > 0:
-                    prices_calculated.append(1)
-                else: prices_calculated.append(-1)
+                prices_calculated.append(prices[i] - prices[i + 12 * (years - 1)])
 
             for i in range(len(prices_calculated)):
                 result += prices_calculated[i]
 
-            return float(result / len(prices_calculated) * 100)
+            print(self.name + ": " + str(result / len(prices_calculated)))
+
+            return result / len(prices_calculated)
+
 
         return "Not enough data!"
+
+    def getAverageDividend(self):
+        tmp = self.ticker.dividends[::-1]
+        result = 0
+
+        if len(tmp) > 0 and len(tmp) >= self.years * 4:
+            for i in range(self.years * 4):
+                result += float(tmp.iloc[i])
+
+            result = (result / (self.years * 4))
+
+        print(self.name + ": " + str(result))
+        return result
 
 def exportToExcell():
     pass            
@@ -67,8 +102,7 @@ def main():
     stock_name_list = file.read().split("\n")
 
     for (index, value) in enumerate(stock_name_list):
-        print("\nFetching data for: " + value)
-        stock_list.append(Stock(value))
+        stock_list.append(Stock(value, 5))
         
 
 if __name__ == "__main__":
